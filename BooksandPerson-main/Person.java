99442a096a58feff9f41634ac1e9361653260713
Ekechi123 <your_email@example.com>
2024-11-26 15:11:30 -0500

    Added abstract Person class with common properties and methods for name and ID

--- BooksandPerson-main/Person.java
+++ BooksandPerson-main/Person.java
@@ -1,16 +1,24 @@
 public abstract class Person {
-    protected String name;
-    protected String id;
+    private String name;
+    private String id;
 
     public Person(String name, String id) {
         this.name = name;
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
     public abstract String describe();
 
     @Override
     public String toString() {
-        return name + " (ID: " + id + ")";
+        return String.format("%s (ID: %s)", name, id);
     }
 }
